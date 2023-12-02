bfa869c96edfe145f8fc348f9c0b49acea16343f
stbeaumont98 <spence.beaumont@gmail.com>
2023-12-01 22:36:10 -0500

    fixed main menu bouncing animation, added space/stars tiles

--- Assets/MenuAssets/bouncePlayer.cs
+++ Assets/MenuAssets/bouncePlayer.cs
@@ -1,28 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class bouncePlayer : MonoBehaviour
 {
 
     private Rigidbody2D p;
+    private Vector2 lastVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
         p = GetComponent<Rigidbody2D>();
-        p.velocity = new Vector2(0.5f,0.5f);
+        p.AddForce(new Vector2(Random.Range(2, 4) * (Random.Range(0, 2) < 1 ? -10 : 10), Random.Range(2, 4) * (Random.Range(0, 2) < 1 ? -10 : 10)));
+        p.AddTorque(0.4f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (p.velocity.x == 0) {
-            p.velocity = new Vector2(0.5f, p.velocity.y);
-        }
-        if (p.velocity.y == 0)
-        {
-            p.velocity = new Vector2(p.velocity.x, 0.5f);
-        }
+        lastVelocity = p.velocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        p.velocity = Vector2.Reflect(lastVelocity, collision.contacts[0].normal);
     }
 }
