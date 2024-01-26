4c076bc3f91d99e27ee7291372455ca2583ff266
stbeaumont98 <spence.beaumont@gmail.com>
2024-01-26 18:09:12 -0500

    Added useful comments to unit/behavioral tests

--- Assets/Tests/EditMode/UnitTests.cs
+++ Assets/Tests/EditMode/UnitTests.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 public class UnitTests
 {
     [Test]
-    public void VolumeString()
+    public void VolumeToStringTest()
     {
+        // Initialize vars
         var gameObject = new GameObject();
         var soundSettings = gameObject.AddComponent<SoundSettings>();
 
-        var volume = soundSettings.VolumeToString(50f);
+        // The VolumeToString function should round down the float value
+        // to the nearest integer, convert it to a string, and append a
+        // '%' symbol.
+        var volume = soundSettings.VolumeToString(50.5f);
 
+        // We should be getting the value "50%"
         Assert.AreEqual("50%", volume);
     }
 }
