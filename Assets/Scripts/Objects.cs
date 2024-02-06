95219a7828fff05c6ac83f855e497627762f181b
stbeaumont98 <spence.beaumont@gmail.com>
2024-02-05 20:35:40 -0500

    Made some improvements to the Objects script, some bugs still need to be fixed

--- Assets/Scripts/Objects.cs
+++ Assets/Scripts/Objects.cs
@@ -7,35 +7,57 @@ public class Objects : MonoBehaviour
 {
     [SerializeField]
     private Transform grabPoint;
-
-    private GameObject grabbedObject;
+    private GameObject gObject;
+    private GameObject tObject;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
 
     
     void Start()
     {
-        grabbedObject = null;
+        gObject = null;
+        tObject = null;
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
-    {
+    {   
+        if (sprite.flipX)
+            grabPoint.localPosition = new Vector3(-0.5f, 0, 0);
+        else
+            gObject.transform.position = grabPoint.localPosition = new Vector3(0.5f, 0, 0);
+
+
+        if (gObject != null)
+            gObject.transform.position = grabPoint.position;
+
+        if (Input.GetKeyDown(KeyCode.E) && gObject != tObject)
+        {
+            gObject = tObject;
+            gObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            gObject.transform.SetParent(transform);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            gObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            gObject.transform.SetParent(null);
+            gObject.GetComponent<Rigidbody2D>().velocity = new Vector2(rb.velocity.x, rb.velocity.y);
+            gObject = null;
+        }
+
+        Debug.Log("Touched object: " + tObject.ToString());
     }
 
-    private void OnTriggerStay2D(Collider2D collider)
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.gameObject.CompareTag("Object")) {
-            if (Input.GetKeyDown(KeyCode.E) && grabbedObject == null)
-            {
-                grabbedObject = collider.transform.parent.gameObject;
-                grabbedObject.GetComponent<Rigidbody2D>().isKinematic = true;
-                grabbedObject.transform.position = grabPoint.position;
-                grabbedObject.transform.SetParent(transform);
-            }
-            else if (Input.GetKeyDown(KeyCode.E))
-            {
-                grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
-                grabbedObject.transform.SetParent(null);
-                grabbedObject = null;
-            }
-        }
+        if (collider.gameObject.CompareTag("Object"))
+            tObject = collider.transform.parent.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) 
+    {
+        if (collider.gameObject.CompareTag("Object"))
+            tObject = null;
     }
 }
