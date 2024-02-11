209ac7cd390b5647386e51b5de4b43228951df4a
stbeaumont98 <spence.beaumont@gmail.com>
2024-02-11 18:44:14 -0500

    Reorganized and added support for horizontal gravity.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer sprite;
     private bool isOnGround;
     private bool isRunning = false;
+    private bool horizGravity;
     private Vector3 localScale;
     [SerializeField] float jumpPower = 10f;
     [SerializeField] float runSpeedMultiplier = 2f;
@@ -31,16 +32,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        horizGravity = Physics2D.gravity.y == 0f;
+
         isRunning = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-        Move(horizontalInput, isRunning);
+
+        Move(Input.GetAxis(horizGravity ? "Vertical" : "Horizontal"), isRunning);
 
         if (Input.GetButtonDown("Jump") && isOnGround == true)
             Jump(jumpPower);
-        else if (Input.GetButtonDown("Vertical") && isOnGround == true)
-        {
-            ReverseGravityY();
-        }
+        else if (Input.GetButtonDown(horizGravity ? "Horizontal" : "Vertical") && isOnGround == true)
+            ReverseGravity();
 
         // if you're not touching the ground, the player sprite should be jumping 
         animator.SetBool("isJumping", !isOnGround);
@@ -75,15 +76,29 @@ public class PlayerMovement : MonoBehaviour
     private void FlipY()
     {
         Vector3 scale = transform.localScale;
-        if ((scale.y < 0 && Physics2D.gravity.y < 0) || (scale.y > 0 && Physics2D.gravity.y > 0))
-            scale.y *= -1;
+        if (horizGravity)
+        {
+            if ((scale.y > 0 && Physics2D.gravity.x < 0) || (scale.y < 0 && Physics2D.gravity.x > 0))
+                scale.y *= -1;
+            transform.localRotation = Quaternion.Euler(0, 0, 90);
+        }
+        else
+        {
+            if ((scale.y < 0 && Physics2D.gravity.y < 0) || (scale.y > 0 && Physics2D.gravity.y > 0))
+                scale.y *= -1;
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
         transform.localScale = scale;
     }
 
     // Handle's the player's left and right movement
     public void Move(float dirX, bool isRunning) {
         float speed = isRunning ? walkSpeed * runSpeedMultiplier : walkSpeed;
-        rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
+
+        if (horizGravity)
+            rb.velocity = new Vector2(rb.velocity.x, dirX * speed);
+        else
+            rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
 
         // if the player is moving left or right, start the running animation
         animator.SetBool("isRunning", dirX != 0);
@@ -110,14 +125,26 @@ public class PlayerMovement : MonoBehaviour
     public void Jump(float power)
     {
         jump.Play();
-        rb.velocity = new Vector2(rb.velocity.x, -GetSign(Physics2D.gravity.y) * power);
+        if (horizGravity)
+            rb.velocity = new Vector2(-GetSign(Physics2D.gravity.x) * power, rb.velocity.y);
+        else
+            rb.velocity = new Vector2(rb.velocity.x, -GetSign(Physics2D.gravity.y) * power);
     }
 
-    public void ReverseGravityY()
+    public void ReverseGravity()
     {
         reverseGrav.Play();
         // Reverse global gravity (for player and objects)
-        Physics2D.gravity = new Vector2(Physics2D.gravity.x, -Physics2D.gravity.y);
+        if (horizGravity)
+            Physics2D.gravity = new Vector2(-Physics2D.gravity.x, Physics2D.gravity.y);
+        else
+            Physics2D.gravity = new Vector2(Physics2D.gravity.x, -Physics2D.gravity.y);
+    }
+
+    public void RotateGravity()
+    {
+        reverseGrav.Play();
+        Physics2D.gravity = new Vector2(-Physics2D.gravity.y, Physics2D.gravity.x);
     }
 
     public Vector2 GetPosition() {
