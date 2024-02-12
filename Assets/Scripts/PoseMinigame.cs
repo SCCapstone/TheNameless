6aa2a7c5f473b2108c729e363e3c6e2c9099525c
Caleb Martin <cam228@outlook.com>
2024-02-11 20:14:35 -0500

    Minigame flip

--- Assets/Scripts/PoseMinigame.cs
+++ Assets/Scripts/PoseMinigame.cs
@@ -12,6 +12,8 @@ public class PoseMinigame : MonoBehaviour
     public GameObject instruction;
     private Transform exTransform;
     private SpriteRenderer exSprite;
+    //Reference to the player's sprite renderer
+    private SpriteRenderer SR;
     //How much time to complete each stage
     public int stageTime;
     //How much time in current stage
@@ -23,31 +25,53 @@ public class PoseMinigame : MonoBehaviour
     public bool requireFlip;
     public bool requirePose;
 
+    //Number of stages in this version of the minigame
     public int numStages;
 
+    //Goal Targets
     public int rotError;
     public float[] goalRotation;
     public bool[] goalMustMove;
     public bool[] goalFlip;
-    public int[] goalPose; 
+    public int[] goalPose;
+
+    //References to scanning hazards
+    public GameObject scan1;
+    public GameObject scan2;
+    public GameObject scan3;
+    public GameObject scan4;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Get components
         exTransform = instruction.GetComponent<Transform>();
         exSprite = instruction.GetComponent<SpriteRenderer>();
+        SR = GetComponent<SpriteRenderer>();
 
+        //Start the minigame
         StartCoroutine(PlayMinigame());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Vertical"))
+        {
+            float vert = Input.GetAxis("Vertical");
+            //Flip
+            if(requireFlip && vert > 0) SR.flipX = !SR.flipX;
+            //Pose
+            if(requirePose && vert < 0)
+            {
+
+            }
+        }
     }
 
     public IEnumerator PlayMinigame()
     {
+        //Buffer before game starts
         yield return new WaitForSeconds(1);
         
         //Loop for each stage
@@ -60,7 +84,7 @@ public class PoseMinigame : MonoBehaviour
             }
             if(requireFlip)
             {
-
+                exSprite.flipX = goalFlip[i];
             }
             if(requirePose)
             {
@@ -78,8 +102,29 @@ public class PoseMinigame : MonoBehaviour
             //Check for failure
             if(requireRotation && (checkedRot<goalRotation[i]-rotError || checkedRot>goalRotation[i]+rotError))
             {
-                Debug.Log(checkedRot);
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+            if(exSprite.flipX != SR.flipX)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+
+            //Activate the moving hazard
+            if(requireMove)
+            {
+                switch(Random.Range(1,5))
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        break;
+                }
             }
 
             yield return new WaitForSeconds(0.5f);
