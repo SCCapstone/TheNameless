8c5521d72c84b700c42375012bd5517cb7a4761e
stbeaumont98 <spence.beaumont@gmail.com>
2024-02-21 20:32:36 -0500

    Added a script for horizontal moving platforms which bounce off walls

---
+++ Assets/Scripts/HorizontalMovingPlatform.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private float velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + velocity, transform.position.y, transform.position.z);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+            velocity = -velocity;
+        
+        if (collision.gameObject.CompareTag("Player"))
+            collision.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
