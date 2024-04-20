1e8c3dd35eed26a73d1e3160351fe575b7a6a83a
Spencer Beaumont <spence.beaumont@gmail.com>
2024-04-20 18:55:16 -0400

    Re-enabled PlayMode tests #84

--- Assets/Tests/PlayMode/MovementTests.cs
+++ Assets/Tests/PlayMode/MovementTests.cs
@@ -8,69 +8,69 @@ public class MovementTests
 {
     // REMOVE COMMENTS TO RUN TESTS
 
-    // [UnityTest]
-    // public IEnumerator MoveLeft()
-    // {
-    //     // Open the 'Testing' scene
-    //     SceneManager.LoadScene("Testing");
-    //     yield return null;
+    [UnityTest]
+    public IEnumerator MoveLeft()
+    {
+        // Open the 'Testing' scene
+        SceneManager.LoadScene("Testing");
+        yield return null;
 
-    //     // Get the player object and make sure it's not null
-    //     var playerObject = GameObject.FindGameObjectWithTag("Player");
-    //     Assert.IsNotNull(playerObject);
+        // Get the player object and make sure it's not null
+        var playerObject = GameObject.FindGameObjectWithTag("Player");
+        Assert.IsNotNull(playerObject);
         
-    //     // Get the instance of PlayerMovement that's applied to the player
-    //     // object and make sure it's not null
-    //     var player = playerObject.GetComponent<PlayerMovement>();
-    //     Assert.IsNotNull(player);
+        // Get the instance of PlayerMovement that's applied to the player
+        // object and make sure it's not null
+        var player = playerObject.GetComponent<PlayerMovement>();
+        Assert.IsNotNull(player);
         
-    //     // Allow the player half a second to land on the ground in the scene
-    //     yield return new WaitForSeconds(0.5f);
+        // Allow the player half a second to land on the ground in the scene
+        yield return new WaitForSeconds(0.5f);
 
-    //     // Get the user's initial position for comparison
-    //     var initialPosition = player.GetPosition();
+        // Get the user's initial position for comparison
+        var initialPosition = player.GetPosition();
 
-    //     // In the period of half a second, simulate a '<-' button press
-    //     for (float t = 0f; t < 0.5f; t += Time.deltaTime) {
-    //         yield return null;
-    //         player.Move(-1, false);
-    //     }
+        // In the period of half a second, simulate a '<-' button press
+        for (float t = 0f; t < 0.5f; t += Time.deltaTime) {
+            yield return null;
+            player.Move(-1, false);
+        }
 
-    //     // Check that the player's position is to the left of their initial position
-    //     Assert.IsTrue(player.GetPosition().x < initialPosition.x);
+        // Check that the player's position is to the left of their initial position
+        Assert.IsTrue(player.GetPosition().x < initialPosition.x);
 
-    // }
+    }
 
     
-    // [UnityTest]
-    // public IEnumerator MoveRight()
-    // {
-    //     // Open the 'Testing' scene
-    //     SceneManager.LoadScene("Testing");
-    //     yield return null;
+    [UnityTest]
+    public IEnumerator MoveRight()
+    {
+        // Open the 'Testing' scene
+        SceneManager.LoadScene("Testing");
+        yield return null;
 
-    //     // Get the player object and make sure it's not null
-    //     var playerObject = GameObject.FindGameObjectWithTag("Player");
-    //     Assert.IsNotNull(playerObject);
+        // Get the player object and make sure it's not null
+        var playerObject = GameObject.FindGameObjectWithTag("Player");
+        Assert.IsNotNull(playerObject);
         
-    //     // Get the instance of PlayerMovement that's applied to the player
-    //     // object and make sure it's not null
-    //     var player = playerObject.GetComponent<PlayerMovement>();
-    //     Assert.IsNotNull(player);
+        // Get the instance of PlayerMovement that's applied to the player
+        // object and make sure it's not null
+        var player = playerObject.GetComponent<PlayerMovement>();
+        Assert.IsNotNull(player);
 
-    //     // Allow the player half a second to land on the ground in the scene
-    //     yield return new WaitForSeconds(0.5f);
+        // Allow the player half a second to land on the ground in the scene
+        yield return new WaitForSeconds(0.5f);
 
-    //     // Get the user's initial position for comparison
-    //     var initialPosition = player.GetPosition();
+        // Get the user's initial position for comparison
+        var initialPosition = player.GetPosition();
 
-    //     // In the period of half a second, simulate a '->' button press
-    //     for (float t = 0f; t < 0.5f; t += Time.deltaTime) {
-    //         yield return null;
-    //         player.Move(1, false);
-    //     }
+        // In the period of half a second, simulate a '->' button press
+        for (float t = 0f; t < 0.5f; t += Time.deltaTime) {
+            yield return null;
+            player.Move(1, false);
+        }
 
-    //     // Check that the player's position is to the right of their initial position
-    //     Assert.IsTrue(player.GetPosition().x > initialPosition.x);
-    // }
+        // Check that the player's position is to the right of their initial position
+        Assert.IsTrue(player.GetPosition().x > initialPosition.x);
+    }
 }
